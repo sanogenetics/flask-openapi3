b124dc6a2b56ce86be441c5cb1bd2c893d4202f9
ismailmo1 <ismail@sanogenetics.com>
2025-05-02 10:38:43 +0100

    add const keyword to schema object

--- tests/test_sano.py
+++ tests/test_sano.py
@@ -1,18 +1,45 @@
 """test additional functionality added to the sano fork of flask_openapi3"""
+
 from http import HTTPStatus
+from typing import Literal
+
+import pydantic
 from flask_openapi3 import OpenAPI
 from flask import Flask, Blueprint, Response
 
-def test_flask_responses():
+
+def test_flask_responses() -> None:
     sano_flask = Flask(__name__)
     blueprint = Blueprint("test", __name__)
 
     @blueprint.route("/hello")
     def handler() -> Response:
         return Response(status=HTTPStatus.OK)
-    
+
     sano_flask.register_blueprint(blueprint)
     api = OpenAPI(__name__)
     api.collect_metadata(sano_flask)
     api.generate_spec_json()
-    assert api.spec_json['paths']['/hello']['get']['responses'] == {'200': {'description': 'OK'}}
+    assert api.spec_json["paths"]["/hello"]["get"]["responses"] == {"200": {"description": "OK"}}
+
+
+def test_const_for_literals() -> None:
+    sano_flask = Flask(__name__)
+    blueprint = Blueprint("test", __name__)
+
+    class Body1(pydantic.BaseModel):
+        name: Literal["i-am-literal"] = "i-am-literal"
+
+    @blueprint.post("/hello")
+    def handler(body: Body1) -> Response:
+        return Response(status=HTTPStatus.OK)
+
+    sano_flask.register_blueprint(blueprint)
+    api = OpenAPI(__name__)
+    api.collect_metadata(sano_flask)
+    api.generate_spec_json()
+    assert api.spec_json["components"]["schemas"]["Body1"] == {
+        "title": "Body1",
+        "type": "object",
+        "properties": {"name": {"title": "Name", "type": "string", "default": "i-am-literal", "const": "i-am-literal"}},
+    }
