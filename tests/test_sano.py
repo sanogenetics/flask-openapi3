907928e19cdc188eb4325c49be2a454d210899d2
jonathanmach <jonathan@sanogenetics.com>
2025-09-08 16:05:31 +0100

    refactor get_responses function to improve schema normalization and handle name conflicts

--- tests/test_sano.py
+++ tests/test_sano.py
@@ -6,6 +6,78 @@ from typing import Literal
 import pydantic
 from flask_openapi3 import OpenAPI
 from flask import Flask, Blueprint, Response
+import pytest
+
+def test_response_name_conflicts() -> None:
+    app = Flask(__name__)
+
+    class APIResponse(pydantic.BaseModel): ...
+
+    @app.post("/hello")
+    def handler1() -> APIResponse:
+        return APIResponse()
+
+
+    # Define a different Response class with the same name
+    class DataPayload(pydantic.BaseModel):
+        title: str
+    class APIResponse(pydantic.BaseModel):
+        foo: DataPayload
+
+    @app.post("/goodbye")
+    def handler2() -> APIResponse:
+        return APIResponse(foo=DataPayload(title="Goodbye"))
+
+    api = OpenAPI(__name__)
+    api.collect_metadata(app)
+    api.generate_spec_json()
+
+    components_schemas = api.spec_json["components"]["schemas"]
+    assert "APIResponse" in components_schemas
+    assert "APIResponse_1" in components_schemas
+
+    assert components_schemas["APIResponse"] == {
+        "title": "APIResponse",
+        "type": "object",
+        "properties": {},
+    }
+    assert components_schemas["APIResponse_1"] == {
+        "title": "APIResponse",
+        "type": "object",
+        "properties": {"foo": {"$ref": "#/components/schemas/DataPayload"}},
+        "required": ["foo"],
+    }
+    assert components_schemas["DataPayload"] == {
+        "title": "DataPayload",
+        "type": "object",
+        "properties": {"title": {"title": "Title", "type": "string"}},
+        "required": ["title"],
+    }
+
+# TODO (SAT-1773): fix remaining name conflicts
+@pytest.mark.skip(reason="Name conflict handling not fully implemented yet (SAT-1773)")
+def test_parse_body_name_conflicts() -> None:
+    app = Flask(__name__)
+
+    class Body(pydantic.BaseModel):
+        name: str
+
+    @app.post("/hello")
+    def handler1(body: Body) -> Response:
+        return Response(status=HTTPStatus.OK)
+    
+    class Body(pydantic.BaseModel):
+        title: str
+
+    @app.post("/goodbye")
+    def handler2(body: Body) -> Response:
+        return Response(status=HTTPStatus.OK)
+
+    api = OpenAPI(__name__)
+    api.collect_metadata(app)
+    api.generate_spec_json()
+    assert "Body" in api.spec_json["components"]["schemas"]
+    assert "Body_1" in api.spec_json["components"]["schemas"]
 
 
 def test_flask_responses() -> None:
